1b5d676a8115e153bb0605520523d581f9e51b4f
frankfc95 <70614293+frankfc95@users.noreply.github.com>
2020-10-22 14:00:06 -0600

    Modificaciones Vistas

--- app/Http/Controllers/clinica/sistema/FichaMedicaNController.php
+++ app/Http/Controllers/clinica/sistema/FichaMedicaNController.php
@@ -167,7 +167,15 @@ class FichaMedicaNController extends Controller
      */
     public function show(FichaMedicaN $fichaMedicaN)
     {
-        //
+        try {
+            
+        } catch (\Exception $th) {
+            if ($th instanceof QueryException) {
+                return redirect()->route('home')->with('danger', 'Error de base de datos');
+            } else {
+                return redirect()->route('home')->with('danger', $th->getMessage());
+            }
+        }
     }
 
     /**
@@ -178,7 +186,15 @@ class FichaMedicaNController extends Controller
      */
     public function edit(FichaMedicaN $fichaMedicaN)
     {
-        //
+        try {
+            return view('clinica.sistema.fichaMedicaN.edit', compact('fichaMedicaN'));
+        } catch (\Exception $th) {
+            if ($th instanceof QueryException) {
+                return redirect()->route('home')->with('danger', 'Error de base de datos');
+            } else {
+                return redirect()->route('home')->with('danger', $th->getMessage());
+            }
+        }
     }
 
     /**
@@ -190,25 +206,101 @@ class FichaMedicaNController extends Controller
      */
     public function update(Request $request, FichaMedicaN $fichaMedicaN)
     {
-        $fichaMedicaN->fecha = date('Y-m-d', strtotime($request->fecha));
-        $fichaMedicaN->padre = $request->padre;
-        $fichaMedicaN->madre = $request->madre;
-        $fichaMedicaN->referido = $request->referido;
-        $fichaMedicaN->email = $request->email;
-        $fichaMedicaN->lugar_nacimiento = $request->lugar_nacimiento;
-        $fichaMedicaN->foto = null;
-        $fichaMedicaN->municipio_id = $request->municipio_id;
-        $fichaMedicaN->persona_id = $request->persona_id;
-        $fichaMedicaN->parto_id = $request->parto_id;
-        $fichaMedicaN->alimentacion_id = $request->alimentacion_id;
-        $fichaMedicaN->save();
+        $this->validate(
+            $request,
+            [
+                'nombre_uno' => 'required|max:20',
+                'nombre_dos' => 'max:30',
+                'apellido_uno' => 'required|max:20',
+                'apellido_dos' => 'max:30',
+                'sexo' => 'required_with:'.Persona::Femenino.','.Persona::Masculino,
+                'fecha_nacimiento' => 'required|date_format:d-m-Y',
 
-        $image = $request->file('foto');
-        $nueva = Storage::disk('foto_fmn')->put('/', $image);
-        $fichaMedicaN->foto = $nueva;
-        $fichaMedicaN->save();
+                'fecha' => 'required|date_format:d-m-Y',
+                'padre' => 'max:100',
+                'madre' => 'max:100',
+                'referido' => 'max:100',
+                'email' => 'nullable|max:50|email',
+                'lugar_nacimiento' => 'max:100',
+                'municipio_id' => 'required|integer|exists:municipio,id',
+                'parto_id' => 'required|integer|exists:parto,id',
+                'alimentacion_id' => 'required|integer|exists:alimentacion,id',
 
-        return response()->json(["Registro" => $fichaMedicaN, "Mensaje" => "Felicidades actualizaste"]);
+                'telefono_uno' => 'nullable|digits_between:8,8',
+                'telefono_dos' => 'nullable|digits_between:8,8',
+                'telefono_tres' => 'nullable|digits_between:8,8',
+            ]
+        );
+
+        try {
+            DB::beginTransaction();
+
+            $persona = new Persona();
+            $persona->nombre_uno = $request->nombre_uno;
+            $persona->nombre_dos = $request->nombre_dos;
+            $persona->apellido_uno = $request->apellido_uno;
+            $persona->apellido_dos = $request->apellido_dos;
+            $persona->sexo = $request->sexo;
+            $persona->fecha_nacimiento = date('Y-m-d', strtotime($request->fecha_nacimiento));
+            
+
+            $fichaMedicaN->fecha = date('Y-m-d', strtotime($request->fecha));
+            $fichaMedicaN->padre = $request->padre;
+            $fichaMedicaN->madre = $request->madre;
+            $fichaMedicaN->referido = $request->referido;
+            $fichaMedicaN->email = $request->email;
+            $fichaMedicaN->lugar_nacimiento = $request->lugar_nacimiento;
+            $fichaMedicaN->foto = null;
+            $fichaMedicaN->municipio_id = $request->municipio_id;
+            $fichaMedicaN->persona_id = $persona->id;
+            $fichaMedicaN->parto_id = $request->parto_id;
+            $fichaMedicaN->alimentacion_id = $request->alimentacion_id;
+
+
+            if(!is_null($request->telefono_uno)) {
+                $telefonouno = new TelefonoFMN();
+                $telefonouno->numero = $request->telefono_uno;
+                $telefonouno->ficha_medica_n_id = $fichaMedicaN->id;
+                
+            }
+
+            if (!is_null($request->telefono_dos)) {
+                $telefonodos = new TelefonoFMN();
+                $telefonodos->numero = $request->telefono_dos;
+                $telefonodos->fichaMedicaN_medica_n_id = $fichaMedicaN->id;
+                
+            }
+
+            if (!is_null($request->telefono_tres)) {
+                $telefonotres = new TelefonoFMN();
+                $telefonotres->numero = $request->telefono_tres;
+                $telefonotres->fichaMedicaN_medica_n_id = $fichaMedicaN->id;
+                
+            }
+
+            /*$image = $request->file('foto');
+            $nueva = Storage::disk('foto_fmn')->put('/', $image);
+            $insert->foto = $nueva;
+            $insert->save();*/
+
+            DB::commit();
+            if (!$fichaMedicaN->isDirty())
+                return redirect()->route('fichaMedicaN.edit', $fichaMedicaN->id)->with('warning', '¡No existe información nueva para actualizar!');
+
+            $persona->save();
+            $fichaMedicaN->save();
+            $telefonouno->save();
+            $telefonodos->save();
+            $telefonotres->save();
+
+            return redirect()->route('fichaMedicaN.index')->with('success', '¡Registro actualizado satisfactoriamente!');
+        } catch (\Exception $th) {
+            if($th instanceof QueryException) {
+                return redirect()->route('fichaMedicaN.edit', $fichaMedicaN->id)->with('danger', 'Error de base de datos');
+            } else {
+                return redirect()->route('fichaMedicaN.edit', $fichaMedicaN->id)->with('danger', $th->getMessage());
+            }
+        }
     }
 
     /**
@@ -219,8 +311,16 @@ class FichaMedicaNController extends Controller
      */
     public function destroy(FichaMedicaN $fichaMedicaN)
     {
-        $fichaMedicaN->delete();
+        try {
+            $fichaMedicaN->delete();
 
-        return response()->json(["Registro" => $fichaMedicaN, "Mensaje" => "Felicidades eliminaste"]);
+            return redirect()->route('fichaMedicaN.index')->with('info', '¡Registro eliminado satisfactoriamente!');
+        } catch (\Exception $th) {
+            if ($th instanceof QueryException) {
+                return redirect()->route('home')->with('danger', 'Error de base de datos');
+            } else {
+                return redirect()->route('home')->with('danger', $th->getMessage());
+            }
+        }
     }
 }
